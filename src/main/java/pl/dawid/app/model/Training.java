a4d12de619842f85be3dd809b09ab2bc23365880
GrubeB <33655111+GrubeB@users.noreply.github.com>
2022-07-11 01:24:02 +0200

    add equals and hashCode model

--- src/main/java/pl/dawid/app/model/Training.java
+++ src/main/java/pl/dawid/app/model/Training.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@EqualsAndHashCode
 @Table(name = "tbl_training")
 public class Training extends IdentityModel {
 
